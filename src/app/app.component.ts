ee253bebc583e26eb435ad653fa6e7fc9391a0db
Daniel Schmitz <danieljfa@gmail.com>
2025-01-30 07:55:32 -0300

    instalação do ngDoc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgDocNavbarComponent, NgDocRootComponent, NgDocSidebarComponent } from '@ng-doc/app';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgDocNavbarComponent, NgDocSidebarComponent, NgDocRootComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
